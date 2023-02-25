c1b21aa35912b9c1901a2ca2e44167c03cd09acd
codehomie1 <mohammedhmohamed02@gmail.com>
2023-02-25 15:32:22 -0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import Header from './components/Header'
+
+function App() {
+  
+
+  return (
+    <div id="root">
+      <div className="App">
+        <div className='Header'>
+        <Header />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+
+    return (
+        <header>
+            <h1>Donation Community Board</h1>
+        </header>
+    )
+}
+
+
+export default Header;
